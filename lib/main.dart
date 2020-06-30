daad16abc370bbae7ada47e3bc39cb25c1a84026
yasinakca <yasin.akca@bil.omu.edu.tr>
2020-06-30 14:33:14 +0300

    conditions and alert

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: MyApp()
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Student Tracking"),
       ),
       body: Center(
-        child: Text("Centered Text"),
+          child: RaisedButton(
+            child: Text("See the result"),
+            onPressed: () {
+              String message;
+              int grade = 50;
+              if(grade > 50) {
+                message = "Passed";
+              }else {
+                message = "Failed";
+              }
+              var alert = AlertDialog(
+                title: Text("Alert"),
+                content: Text(message),
+              );
+              showDialog(context: context, builder: (BuildContext context) => alert);
+            },
+          )
       ),
-    ),
-  ));
+    );
+  }
+
 }
