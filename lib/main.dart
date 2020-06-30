89eb205ab5a1f2d870e6339bb67584decd3671dd
yasinakca <yasin.akca@bil.omu.edu.tr>
2020-06-30 23:39:13 +0300

    reference type, generic list,oop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/student.dart';
+
 void main() {
   runApp(MaterialApp(
     home: MyApp()
@@ -7,45 +9,66 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  List<String> students = ["Yasin Akca","Emir Parlak","Yigit Caglar"];
+  List<Student> students = [
+    Student("Yasin","Akca",60),
+    Student("Emir","Parlak",40)
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Student Tracking"),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-                itemCount: students.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Text(students[index]);
-                }
-            ),
-          ),
-          Center(
-              child: RaisedButton(
-                child: Text("See the result"),
-                onPressed: () {
-                  String message;
-                  int grade = 50;
-                  if(grade > 50) {
-                    message = "Passed";
-                  }else {
-                    message = "Failed";
-                  }
-                  var alert = AlertDialog(
-                    title: Text("Alert"),
-                    content: Text(message),
-                  );
-                  showDialog(context: context, builder: (BuildContext context) => alert);
-                },
-              )
-          ),
-        ],
-      ),
+      body: buildBody(context)
     );
   }
 
+  Widget buildBody(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+              itemCount: students.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png"),
+                  ),
+                  title: Text(students[index].firstName + " " + students[index].lastName),
+                  subtitle: Text(students[index].grade.toString()),
+                  trailing: buildIcon(students[index].grade),
+                );
+              }
+          ),
+        ),
+        Center(
+            child: RaisedButton(
+              child: Text("See the result"),
+              onPressed: () {
+                String message;
+                int grade = 50;
+                if(grade > 50) {
+                  message = "Passed";
+                }else {
+                  message = "Failed";
+                }
+                var alert = AlertDialog(
+                  title: Text("Alert"),
+                  content: Text(message),
+                );
+                showDialog(context: context, builder: (BuildContext context) => alert);
+              },
+            )
+        ),
+      ],
+    );
+  }
+
+  Widget buildIcon(int grade) {
+    if(grade > 50) {
+      return Icon(Icons.done);
+    }else{
+      return Icon(Icons.cancel);
+    }
+  }
 }
---
+++ lib/models/student.dart
@@ -0,0 +1,11 @@
+class Student{
+  String firstName;
+  String lastName;
+  int grade;
+
+  Student(String firstName, String lastName, int grade) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = grade;
+  }
+}
